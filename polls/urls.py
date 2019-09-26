1a384c627219e7e8c7c2a0eb23a19e03756f5e7d
Brooke Kennison <brookelkennison@gmail.com>
2019-09-26 16:35:05 -0500

    index view working

--- polls/urls.py
+++ polls/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'polls'
+
 urlpatterns = [
-    url('', views.IndexView.as_view(), name="index"),
-    url('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    url('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/
+    url('', views.index, name='index'),
+    # ex: /polls/5/
+    url('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
     url('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.template import loader
 
 from .models import Choice, Question
 
 
-
-
-
 # Create your views here.
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -37,4 +43,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
