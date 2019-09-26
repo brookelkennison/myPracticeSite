5f4a1f437cae6b04595d7a5948f653ed021a0f7d
Brooke Kennison <brookelkennison@gmail.com>
2019-09-26 10:31:09 -0500

    detail and index routes done

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     url('', views.index, name="index"),
+    url('<int:question_id>/', views.detail, name='detail'),
+    url('<int:question_id>/results/', views.results, name='results'),
+    url('<int:question_id>/vote/', views.vote, name='vote'),
 ]
