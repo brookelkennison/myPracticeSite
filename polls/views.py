ffaa11489cbfd816d24215d23c5a5b8a91189da4
Brooke Kennison <brookelkennison@gmail.com>
2019-09-27 18:40:05 -0500

    updated views.py

--- polls/views.py
+++ polls/views.py
@@ -16,28 +16,14 @@ from .models import Choice, Question
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-
+    return HttpResponse("You're looking at the question %s." % question_id)
 
 
 def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+    return HttpResponse("You're voting on question %s." % question_id)
