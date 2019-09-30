e44d987956fe5c22686796c024b33bf7ab326fe9
Brooke Kennison <brookelkennison@gmail.com>
2019-09-30 14:48:59 -0500

    edited admin choice text

--- polls/admin.py
+++ polls/admin.py
@@ -3,16 +3,22 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,                  {'fields': ['question_text']}),
-        ('Date information',    {'fields': ['pub_date']}),
+        ('Date information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
